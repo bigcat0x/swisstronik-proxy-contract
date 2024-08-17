8f198746d025efe7a5ca717f13bf470ced301ad4
root <root@DESKTOP-4400F6U>
2024-08-17 19:13:05 +0100

    feat: Done with proxy contract

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x62165b19EC38355328Ddb7341Cf3B5583444A4f2'
+export const ProxyAdmin = '0xb0c6cd23A4f58E2298A11e3206d31CAc3cfA7927'
+export const SWTRImplementation = '0x13946511A3a3a405e715621cDdcc41370A122b95'
+    
